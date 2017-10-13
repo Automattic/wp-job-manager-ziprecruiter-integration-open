49ccf991086468841ee200259658fcf05f0c6d59
Jake Oehler Morrison <jake.morrison@automattic.com>
2017-10-13 15:32:34 +0200

    Add version to template file

--- templates/content-attribution.php
+++ templates/content-attribution.php
@@ -1,3 +1,20 @@
+<?php
+/**
+ * Attribution notice for ZipRecruiter job listings.
+ *
+ * This template can be overridden by copying it to yourtheme/wp-job-manager-ziprecruiter-integration/content-attribution.php.
+ *
+ * @see         https://wpjobmanager.com/document/template-overrides/
+ * @author      Automattic
+ * @package     WP Job Manager - ZipRecruiter Notice
+ * @category    Template
+ * @version     1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
 <li class="wp-job-manager-attribution-row job_listing" id="zr_attributed">
 	<a href="https://www.ziprecruiter.com/jobs" id="jobs_widget_link"><img id="zr_logo" src="https://www.ziprecruiter.com/img/logos/logo-sm-black-304px.png" alt="ZipRecruiter" height="16" style="border: 0; vertical-align: middle;" alt="Job Search by ZipRecruiter" /></a>
 </li>
